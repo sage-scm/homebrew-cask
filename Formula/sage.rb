4a92ba16478e6db20f40b989cfdec28153eb93f8
Sage Smudge <sage.smudge.git@gmail.com>
2025-10-16 09:40:33 +1100

    Update Sage to 2025.10.16 (#11)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Brayden Moon <crazywolf132@gmail.com>

--- Formula/sage.rb
+++ Formula/sage.rb
@@ -1,26 +1,26 @@
 class Sage < Formula
   desc "🌿 Sage — A Git workflow tool for managing branches and commits"
   homepage "https://github.com/sage-scm/sage"
-  version "2025.10.14"
+  version "2025.10.16"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/sage-scm/sage/releases/download/2025.10.14/sg-macos-amd64.tar.gz"
-      sha256 "6b3bb2024f3b4541a21bd0ff90c70aee3bb142f857e73476b0e25c87d7d083e1"
+      url "https://github.com/sage-scm/sage/releases/download/2025.10.16/sg-macos-amd64.tar.gz"
+      sha256 "fbb71b09e43a4c07d4ccaadac853bdd60e7100ea2c0184ec3b2f64a3e28b8fd5"
     else
-      url "https://github.com/sage-scm/sage/releases/download/2025.10.14/sg-macos-arm64.tar.gz"
-      sha256 "ba3701a83a23cc4d86e2bb733372c187e89fb88fdbadb462260bfc32faeb99a6"
+      url "https://github.com/sage-scm/sage/releases/download/2025.10.16/sg-macos-arm64.tar.gz"
+      sha256 "638a9350e465cd0a48d87e132caf802f89af0dcdbbac838205b87ed337421d4f"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/sage-scm/sage/releases/download/2025.10.14/sg-linux-amd64.tar.gz"
-      sha256 "b8f45c78a4fca65f4a61e81b834564f09bbde9df0be27c658eb14f74bd597494"
+      url "https://github.com/sage-scm/sage/releases/download/2025.10.16/sg-linux-amd64.tar.gz"
+      sha256 "085be5f4867e595bf767dedd2054cf0836b68f26191fd81027cad44ca0a23233"
     else
-      url "https://github.com/sage-scm/sage/releases/download/2025.10.14/sg-linux-arm64.tar.gz"
-      sha256 "23cae1601dbf3225005e945c536f5ec48ffb69d7000638bc2ab39c34d503aec6"
+      url "https://github.com/sage-scm/sage/releases/download/2025.10.16/sg-linux-arm64.tar.gz"
+      sha256 "0de9be295aea11b30b1e76153416461bd1fccdf666bc1040144ab5fb3a4c4c75"
     end
   end
 
